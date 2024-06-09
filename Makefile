eaa9bc078e4758247a0a07d3dcebcfba56869db7
1KgChawal <vikashkumarojha2004@gmail.com>
2024-06-09 09:59:00 +0530

    almost_done

--- Makefile
+++ Makefile
@@ -2,20 +2,23 @@ NVCC=nvcc
 
 FLAG=-g
 
-SOURCE=Matrix.cu main.cpp
-
-OBJECTS=Matrix.o main.o
-
 EXECUTABLE=matrix_multiply.out
 
-%.o: %.c++ %.h
-	$(NVCC) $(FLAG) -o $@ -c $<
+SOURCE=Matrix.cu main.cpp
 
-%.o: %.cu %.h
-	$(NVCC) $(FLAG) -o $@ -c $<
+OBJECTS=Matrix.cu.o main.o Matrix.o
+
+Matrix.cu.o: Matrix.cu
+	$(NVCC) $(FLAG) -o $@ -c $^
+
+Matrix.o: Matrix.h
+	$(NVCC) $(FLAG) -o $@ -c $^
 
 main.o: main.cpp
 	$(NVCC) $(FLAG) -o main.o -c main.cpp
 
 $(EXECUTABLE): $(OBJECTS)
 	$(NVCC) $(FLAG) -o $@ $^
+
+clean:
+	rm -rf *.o *.out
--- Matrix.cu
+++ Matrix.cu
@@ -1,52 +1,5 @@
-#include "Matrix.h"
+#include <iostream>
 
-template <typename T>
-Matrix<T>::Matrix() {
-    rows = 0;
-    cols = 0;
-    vector = nullptr;
-}
-template <typename T>
-Matrix<T>::Matrix(int r, int c) : rows(r), cols(c) {
-    vector = new T[r * c];
-}
-template <typename T>
-T& Matrix<T>::operator()(int r, int c) {
-    return vector[r * cols + c];
-}
-template <typename T>
-const T& Matrix<T>::operator()(int r, int c) const {
-    return vector[r * cols + c];
-}
-template <typename T>
-T* Matrix<T>::data() {
-    return vector;
-}
-template <typename T>
-int Matrix<T>::getRows() {
-    return rows;
-}
-template <typename T>
-int Matrix<T>::getCols() {
-    return cols;
-}
-template <typename T>
-unsigned int Matrix<T>::bytes() {
-    return rows * cols * sizeof(T);
-}
-template<typename T>
-void Matrix<T>::print(){
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<cols;j++){
-            std::cout<<(*this)(i,j)<<" ";
-        }
-        std::cout<<"\n";
-    }
-}
-template <typename T>
-Matrix<T> Matrix<T>::operator*(Matrix<T>& M) {
-    return multiply(*this, M);
-}
 template <typename T>
 __global__ void dkernal(T* d_M, T* d_M1, T* d_M2, int rows, int cols, int r_c) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -58,22 +11,23 @@ __global__ void dkernal(T* d_M, T* d_M1, T* d_M2, int rows, int cols, int r_c) {
         }
     }
 }
-template <typename T>
-Matrix<T> Matrix<T>::multiply(Matrix<T>& M1, Matrix<T>& M2) {
-    Matrix M(M1.getRows(), M2.getCols());
-    int k = M1.getCols();
-    T *d_M1, *d_M2, *d_M;
-    cudaMalloc(&d_M1, M1.bytes());
-    cudaMalloc(&d_M2, M2.bytes());
-    cudaMalloc(&d_M, M.bytes());
-    cudaMemcpy(d_M1, M1.data(), M1.bytes(), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_M2, M2.data(), M2.bytes(), cudaMemcpyHostToDevice);
-    dim3 block(16, 16);
-    dim3 grid((M.getRows() + blockDim.x - 1) / blockDim.x, (M.getCols() + blockDim.y - 1) / blockDim.y);
-    dkernal<<<grid, block>>>(d_M, d_M1, d_M2, M.getRows(), M.getCols(), k);
-    cudaMemcpy(M.data(), d_M, M.bytes(), cudaMemcpyDeviceToHost);
+
+template<typename T>
+void cudaMultiply(T* M1,T* M2,T* M,int rows, int cols,int r_c){
+    unsigned int size1=rows*r_c*sizeof(T);
+    unsigned int size2=r_c*cols*sizeof(T);
+    unsigned int size=rows*cols*sizeof(T);
+    T *d_M1,*d_M2,*d_M;
+    cudaMalloc(&d_M,size);
+    cudaMalloc(&d_M1,size1);
+    cudaMalloc(&d_M2,size2);
+    cudaMemcpy(d_M1,M1,size1,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_M2,M2,size2,cudaMemcpyHostToDevice);
+    dim3 block(16,16);
+    dim3 grid((rows+blockDim.x-1)/blockDim.x,(cols+blockDim.y-1)/blockDim.y);
+    dkernal<<<grid,block>>>(d_M,d_M1,d_M2,rows,cols,r_c);
+    cudaMemcpy(M,d_M,size,cudaMemcpyDeviceToHost);
     cudaFree(d_M);
     cudaFree(d_M1);
     cudaFree(d_M2);
-    return M;
 }
