7a1c4c091d02e4c4c76d2dc1df04824272bc3755
1KgChawal <vikashkumarojha2004@gmail.com>
2024-06-11 12:31:44 +0530

    v1

--- cudaFunction.cu
+++ cudaFunction.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "cudaFunction.h.cu"
+#include "Error.cu"
 
 template <typename T>
 __global__ void dkernal(T* d_M, T* d_M1, T* d_M2, int rows, int cols, int r_c) {
@@ -19,16 +20,18 @@ void cudaMultiply(T* M1,T* M2,T* M,int rows, int cols,int r_c){
     unsigned int size2=r_c*cols*sizeof(T);
     unsigned int size=rows*cols*sizeof(T);
     T *d_M1,*d_M2,*d_M;
-    cudaMalloc(&d_M,size);
-    cudaMalloc(&d_M1,size1);
-    cudaMalloc(&d_M2,size2);
-    cudaMemcpy(d_M1,M1,size1,cudaMemcpyHostToDevice);
-    cudaMemcpy(d_M2,M2,size2,cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR(cudaMalloc(&d_M,size));
+    CHECK_CUDA_ERROR(cudaMalloc(&d_M1,size1));
+    CHECK_CUDA_ERROR(cudaMalloc(&d_M2,size2));
+    CHECK_CUDA_ERROR(cudaMemcpy(d_M1,M1,size1,cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaMemcpy(d_M2,M2,size2,cudaMemcpyHostToDevice));
     dim3 block(16,16);
     dim3 grid((rows+block.x-1)/block.x,(cols+block.y-1)/block.y);
     dkernal<<<grid,block>>>(d_M,d_M1,d_M2,rows,cols,r_c);
-    cudaMemcpy(M,d_M,size,cudaMemcpyDeviceToHost);
-    cudaFree(d_M);
-    cudaFree(d_M1);
-    cudaFree(d_M2);
+    CHECK_CUDA_ERROR(cudaGetLastError());
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+    CHECK_CUDA_ERROR(cudaMemcpy(M,d_M,size,cudaMemcpyDeviceToHost));
+    CHECK_CUDA_ERROR(cudaFree(d_M));
+    CHECK_CUDA_ERROR(cudaFree(d_M1));
+    CHECK_CUDA_ERROR(cudaFree(d_M2));
 }
