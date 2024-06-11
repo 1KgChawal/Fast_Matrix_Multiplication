4ac97e60e00f696ef05e71850dd80bb5148d5267
1KgChawal <vikashkumarojha2004@gmail.com>
2024-06-11 12:24:55 +0530

    v1

---
+++ Error.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void CHECK_CUDA_ERROR(cudaError_t err) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA Error : " << cudaGetErrorString(err) << " at " << __FILE__ << " : " << __LINE__ << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
